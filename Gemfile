46c0c62fab552de5e95ef149e7dc2f34425c42f7
Andrew Serputko <aserputko@gmail.com>
2013-05-15 22:43:09 +0300

    Init project

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source "http://rubygems.org"
+gem "pg", "0.12.2"
+gem "rails", "3.2.13"
+gem "mechanize", "~> 2.6.0"
+
+gem "jquery-rails", "2.0.2"
+gem "bootstrap-sass", "2.1"
+gem "therubyracer"
+
+group :assets do
+  gem "sass-rails", "~> 3.2.5"
+	gem "coffee-rails", "~> 3.2.2"
+  gem "uglifier", "1.2.3"
+end
+
+group :development do
+  gem "rspec-rails", "2.11.0"
+end
+
+group :test do
+  gem "rspec-rails", "2.11.0"
+  gem "capybara", "1.1.2"
+end
+
+gem "pg_search"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recognizer::Application.config.secret_token = '2c2da45c34c5a441423051dce2911cd6f6ef8eb29d65dbaaf9dd6fc34910cc55c87f4a23266dac24bf0695e89af7185159a59ec6148de46c9e96c9ffe6cd440d'
